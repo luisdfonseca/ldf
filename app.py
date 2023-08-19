24b35c00f7b5ec1a2ec07b95972746cd8d09b736
Luis Daniel Fonseca <luis.fonseca@hey.com>
2023-08-19 13:00:18 -0700

    adding subscribe form on slider

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-import json, os, requests, pdb
-from flask import Flask, render_template, g, request, send_from_directory
+import json, os, requests, pdb, jwt
+from flask import Flask, render_template, g, request, send_from_directory, jsonify
+from datetime import datetime as date
 
 app= Flask(__name__)
 
 GHOST_API_KEY = os.getenv('GHOST_API_KEY','')
+GHOST_ADMIN_API_KEY = os.getenv('GHOST_ADMIN_API_KEY','')
+
+URL = "https://luis-daniel-fonseca.ghost.io/"
 
 # # Load translations
 with open('translations.json') as json_file:
@@ -31,6 +35,7 @@ def before_request():
     language = 'en'  # default language
     if request.view_args and 'language' in request.view_args:
         language = request.view_args['language']
+    g.language = language
     g.translations = translations.get(language, {})
 
 @app.route('/', defaults={'language': 'en'})
@@ -51,3 +56,59 @@ def index(language):
 def favicon(path):
     return send_from_directory(os.path.join(app.root_path, 'static/images'),
                                'favicon.png', mimetype='image/png')
+
+
+
+@app.route('/subscribe', methods=['POST'])
+def subscribe():
+    language = request.form.get('language', 'en')
+
+    phone = request.form.get('phone')
+    name = request.form.get('name')
+    if name or phone:
+        # This submission is likely a spam bot
+        return "Thanks", 200
+
+
+    # Get email from form
+    email = request.form['email']
+
+    # Split the key into ID and SECRET
+    id, secret = GHOST_ADMIN_API_KEY.split(':')
+    print(id)
+    print(secret)
+    print(email)
+
+    # Prepare header and payload
+    iat = int(date.now().timestamp())
+
+    header = {'alg': 'HS256', 'typ': 'JWT', 'kid': id}
+    payload = {
+        'iat': iat,
+        'exp': iat + 5 * 60,
+        'aud': '/admin/'
+    }
+
+    # Create the token (including decoding secret)
+    token = jwt.encode(payload, bytes.fromhex(secret), algorithm='HS256', headers=header)
+
+    # Make an authenticated request to create a post
+    url = '%s/ghost/api/admin/members/' % URL
+    print(url)
+    headers = {'Authorization': 'Ghost {}'.format(token)}
+    body = {"members": [{"email": email}]}
+
+    r = requests.post(url, json=body, headers=headers)
+
+    print(r)
+    print(r.text)
+
+    translations_temp = translations.get(language, {})
+
+    success_msg = translations_temp['signup-success-message']
+    error_msg = translations_temp['signup-failure-message']
+
+    if r.status_code == 201:  # Assuming 201 is the success status code from the API
+      return jsonify({'status': 'success', 'message': success_msg}), 200
+    else:
+      return jsonify({'status': 'error', 'message': error_msg}), 400
